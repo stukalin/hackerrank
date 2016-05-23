270b9f56d35997e11f4788b34124037cffa44149
Andrey Stukalin <a.stukalin@crossvertise.com>
2016-05-23 16:34:56 +0200

    project euler tasks

---
+++ Hackerrank/Projecteuler/ProjectEuler1.cs
@@ -0,0 +1,64 @@
+﻿namespace Hackerrank.Projecteuler
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Multiples of 3 and 5
+    /// </summary>
+    [TestFixture]
+    public class ProjectEuler1
+    {
+        [TestCase(10, ExpectedResult = 23)]
+        [TestCase(100, ExpectedResult = 2318)]
+        public long Solution(int n)
+        {
+            long three = 0;
+            long five = 0;
+            long fteen = 0;
+
+            if (n <= 3)
+            {
+                three = 0;
+            }
+            else if (n <= 6)
+            {
+                three = 3;
+            }
+            else
+            {
+                long a = (n - 1) / 3;
+                three = (3 + 3 * a) * a / 2;
+            }
+
+            if (n <= 5)
+            {
+                five = 0;
+            }
+            else if (n <= 10)
+            {
+                five = 5;
+            }
+            else
+            {
+                long a = (n - 1) / 5;
+                five = (5 + 5 * a) * a / 2;
+            }
+
+            if (n <= 15)
+            {
+                fteen = 0;
+            }
+            else if (n <= 30)
+            {
+                fteen = 15;
+            }
+            else
+            {
+                long a = (n - 1) / 15;
+                fteen = (15 + 15 * a) * a / 2;
+            }
+
+            return three + five - fteen;
+        }
+    }
+}
---
+++ Hackerrank/Projecteuler/ProjectEuler141.cs
@@ -0,0 +1,63 @@
+﻿namespace Hackerrank.Projecteuler
+{
+    using System;
+    using System.Diagnostics;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Investigating progressive numbers, n, which are also square.
+    /// </summary>
+    [TestFixture]
+    public class ProjectEuler141
+    {
+        [TestCase(58, ExpectedResult = true)]
+        [TestCase(9, ExpectedResult = true)]
+        [TestCase(10404, ExpectedResult = true)]
+        [TestCase(100000000000, ExpectedResult = false)]
+        public bool FindIfProgressive(long n)
+        {
+            return IsProgressive(n);
+        }
+
+        [TestCase(1000000)]
+        public void ListProgressivesUpTo(long n)
+        {
+            for (long i = n; i >= 2; i--)
+            {
+                if (IsProgressive(i))
+                {
+                    continue;
+                    //Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static bool IsProgressive(long n)
+        {
+            bool isProgressive = false;
+            var upperMultiplier = (long)Math.Floor(Math.Sqrt(n));
+
+            for (long m1 = upperMultiplier; m1 >= 2 ; m1--)
+            {                
+                long m2 = n / m1;
+                long rest = n % m1;
+
+                if (rest == 0)
+                {
+                    continue;
+                }
+
+                isProgressive = Math.Abs(m2 / (double)m1 - m1 / (double)rest) < 1e-8;
+
+                if (isProgressive)
+                {
+                    //Console.WriteLine("{0} * {1} + {2} = {3} with factor {4:F}. Time: {5}", m2, m1, rest, n, m2 / (double)m1, sw.ElapsedMilliseconds);
+                    break;
+                }
+            }
+
+            return isProgressive;
+        }
+    }
+}
