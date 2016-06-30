7fec87582c2d3a45bff7d3f879faca7bad52c361
Andrey Stukalin <a.stukalin@crossvertise.com>
2016-06-30 15:07:18 +0200

    WeekOfCode21 #4

--- Hackerrank/WeekOfCode21/DemandingMoney.cs
+++ Hackerrank/WeekOfCode21/DemandingMoney.cs
@@ -11,6 +11,7 @@ namespace Hackerrank.WeekOfCode21
     class DemandingMoney
     {
         private int maxMoney;
+
         private List<List<int>> usedPaths;
 
         private int[] moneys;
@@ -25,23 +26,21 @@ namespace Hackerrank.WeekOfCode21
         [Test]
         public void Test2()
         {
-//            var result = Solution(5, 6, new[] { 100,1,1,1,1 }, new[,] { { 1, 2 }, { 1, 3 }, {1,4}, {1,5},{2,4},{3,4} });
-//            Console.WriteLine(result);
-//            Assert.That(result, Is.EqualTo("100 1"));
-
-            var result = Solution(5, 6, new[] { 0, 0, 0, 0, 0 }, new[,] { { 1, 2 }, { 1, 3 }, { 1, 4 }, { 1, 5 }, { 2, 4 }, { 3, 4 } });
-            Console.WriteLine(result);
+            var result = Solution(5, 6, new[] { 100, 1, 1, 1, 1 }, new[,] { { 1, 2 }, { 1, 3 }, { 1, 4 }, { 1, 5 }, { 2, 4 }, { 3, 4 } });
             Assert.That(result, Is.EqualTo("100 1"));
+
+            result = Solution(5, 6, new[] { 0, 0, 0, 0, 0 }, new[,] { { 1, 2 }, { 1, 3 }, { 1, 4 }, { 1, 5 }, { 2, 4 }, { 3, 4 } });
+            Assert.That(result, Is.EqualTo("0 10"));
         }
 
         public string Solution(int n, int m, int[] cs, int[,] abs)
         {
             maxMoney = 0;
             usedPaths = new List<List<int>>();
-            this.moneys = cs;
+            moneys = cs;
 
             char[,] paths = new char[n, n];
-            for (int i=0;i<n;i++)
+            for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
@@ -57,79 +56,49 @@ namespace Hackerrank.WeekOfCode21
                 paths[abs[i, 1] - 1, abs[i, 0] - 1] = '1';
             }
 
-            for (int i=0;i<n;i++)
+            for (int i = 0; i < n; i++)
             {
-                Step(i, 0, new List<int>(), paths);
+                Step(i, 0, new List<int>(), paths, new List<int>());
             }
 
-            return string.Format("{0} {1}", this.maxMoney, this.usedPaths.Count);
+            return string.Format("{0} {1}", maxMoney, usedPaths.Count);
         }
 
-        public void Step(int row, int sum, List<int> curpath, char[,] paths)
+        public void Step(int row, int sum, List<int> curpath, char[,] paths, List<int> forbidden)
         {
             // add the sum 
-            sum += this.moneys[row];
+            sum += moneys[row];
             curpath.Add(row);
 
             // set visited in corresponding column
             for (int i = 0; i < paths.GetLength(0); i++)
             {
-                if (paths[i, row] == '0')
+                if (paths[row, i] == '1')
                 {
-                    paths[i, row] = 'v';
+                    forbidden.Add(i);
                 }
             }
 
             for (int j = 0; j < paths.GetLength(0); j++)
             {
-                if (paths[row, j] == '0')
+                if (paths[row, j] == '0' && !forbidden.Contains(j))
                 {
-                    paths[row, j] = 'v';
-
-                    // set visited in column to be visited
-                    for (int i = 0; i < paths.GetLength(0); i++)
-                    {
-                        if (paths[i,j] == '0')
-                        {
-                            paths[i, j] = 'v';
-                        }
-                    }
-
                     // make a step
-                    Step(j, sum, curpath, paths);
-
-                    // rollback
-                    paths[row, j] = '0';
-
-                    for (int i = 0; i < paths.GetLength(0); i++)
-                    {
-                        if (paths[i, j] == 'v')
-                        {
-                            paths[i, j] = '0';
-                        }
-                    }
+                    Step(j, sum, curpath, paths, forbidden);
                 }
             }
 
-            Console.WriteLine(string.Join(" ", curpath));
-
-            if (sum > this.maxMoney)
+            if (sum > maxMoney)
             {
-                this.maxMoney = sum;
-                this.usedPaths.Clear();
-                this.usedPaths.Add(curpath.ToList());
-
-                //Console.WriteLine(string.Join(" ", curpath));
-
+                maxMoney = sum;
+                usedPaths.Clear();
+                usedPaths.Add(curpath.ToList());
             }
-            else if (sum == this.maxMoney)
+            else if (sum == maxMoney)
             {
                 if (usedPaths.All(a => a.Except(curpath).Any()))
                 {
-                    this.usedPaths.Add(curpath.ToList());
-
-                    //Console.WriteLine(string.Join(" ", curpath));
-
+                    usedPaths.Add(curpath.ToList());
                 }
             }
 
@@ -139,9 +108,9 @@ namespace Hackerrank.WeekOfCode21
             // set visited in corresponding column
             for (int i = 0; i < paths.GetLength(0); i++)
             {
-                if (paths[i, row] == 'v')
+                if (paths[row, i] == '1')
                 {
-                    paths[i, row] = '0';
+                    forbidden.Remove(i);
                 }
             }
         }
